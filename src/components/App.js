111c7c7d912720f8493b693cb8685c25b1b46c17
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-11-01 15:40:14 +0530

    changes done in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,82 +7,63 @@ const App = () => {
   const [y, setY] = useState(0);
   const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px"
+    top: "0px",
+    position: "absolute"
   });
+  //const [style, setStyle] = useState(ballPosition);
   const reset = () => {
     setRenderBall(false);
     setX(0);
     setY(0);
     setBallPosition({
       left: "0px",
-      top: "0px"
+      top: "0px",
+      position: "absolute"
     });
   };
   const start = () => {
     setRenderBall(true);
   };
+  const handleUserKeyPress = (event) => {
+    console.log(event.key);
+    let newx = x;
+    let newy = y;
+    if (event.keyCode === 39) {
+      newx = x + 5;
+      setX(newx);
+    } else if (event.keyCode === 37) {
+      newx = x - 5;
+      setX(newx);
+    } else if (event.keyCode === 38) {
+      newy = y - 5;
+      setY(newy);
+    } else if (event.keyCode === 40) {
+      newy = y + 5;
+      setY(newy);
+    }
+    console.log(x + " " + y);
+    setBallPosition({
+      left: `${newx}px`,
+      top: `${newy}px`,
+      position: `absolute`
+    });
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", handleUserKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleUserKeyPress);
+    };
+  });
+
   const renderChoice = () => {
-    if (renderBall) {
-      return <div className="ball" style={ballPosition}></div>;
-    } else
+    if (!renderBall) {
       return (
         <button onClick={start} className="start">
           Start
         </button>
       );
+    } else return <div style={ballPosition} className="ball"></div>;
   };
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      if (event.keyCode === 39) {
-        setX(x + 5, () => {
-          setBallPosition((prevValue) => {
-            return {
-              left: `${x}px`,
-              top: prevValue.top
-            };
-          });
-        });
-      }
-
-      if (event.keyCode === 37) {
-        setX(x - 5, () => {
-          setBallPosition((prevValue) => {
-            return {
-              left: `${x}px`,
-              top: prevValue.top
-            };
-          });
-        });
-      }
-
-      if (event.keyCode === 38) {
-        setY(y - 5, () => {
-          setBallPosition((prevValue) => {
-            return {
-              left: prevValue.left,
-              top: `${y}px`
-            };
-          });
-        });
-      }
-
-      if (event.keyCode === 40) {
-        setY(y + 5, () => {
-          setBallPosition((prevValue) => {
-            return {
-              left: prevValue.left,
-              top: `${y}px`
-            };
-          });
-        });
-      }
-    };
-    document.addEventListener("keydown", handleKeyPress);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyPress);
-    };
-  });
 
   return (
     <div className="playground">
